8030c7e1d02c7d65bd95c220003c2be144933c3f
sportline105 <norbert616@freenet.de>
2026-04-11 16:54:33 +0200

    Refactor namespace in CarItemsProcessor.php

--- Classes/Utility/CarItemsProcessor.php
+++ Classes/Utility/CarItemsProcessor.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Nkfire\RescueReports\Utility\Tca;
+namespace Nkfire\RescueReports\Utility;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
